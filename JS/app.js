f4fedcd836047b5af60a96cfac998fc64db1e454
Pedro Binotto <pedrosantibinotto@gmail.com>
2020-06-08 01:08:10 -0300

    footer

--- JS/app.js
+++ JS/app.js
@@ -1,18 +1,31 @@
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navigation_ID");
-var ok = document.getElementById("ok");
 var sticky = navbar.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
-    ok.classList.add("lowered");
-    ok.classList.remove("top_fixed");
   }
   else {
     navbar.classList.remove("sticky");
-    ok.classList.remove("lowered");
-    ok.classList.add("top_fixed");
+  }
+}
+
+var footer = document.getElementById("footer");
+var stickyBottom = window.innerHeight - footer.offsetTop;
+
+window.onload = function() {myFooter()};
+console.log(stickyBottom);
+
+function myFooter() {
+  console.log("**");
+  if (stickyBottom > 0) {
+    console.log("Maior>");
+    footer.classList.add("stickyBottom");
+  }
+  else {
+    console.log("Menor<");
+    footer.classList.remove("stickyBottom");
   }
 }
