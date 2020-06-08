414dc67dd48dcce0284aaee6282afe9af3cd5a7e
Pedro Binotto <pedrosantibinotto@gmail.com>
2020-06-08 17:54:15 -0300

    viewport

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,4 @@
+// Sticky navbar
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navigation_ID");
@@ -12,6 +13,7 @@ function myFunction() {
   }
 }
 
+// Pin footer div to the bottom of the page when content is shorter than the viewport height
 var footer = document.getElementById("footer");
 var stickyBottom = window.innerHeight - footer.offsetTop;
 
